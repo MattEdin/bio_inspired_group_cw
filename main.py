c1d54dc18f596624e1714ec29ced2fc4f719f110
MattEdin <matthew.mccaskell@virginmedia.com>
2025-10-02 19:16:58 +0100

    injesting the data and splitting it into training and testing, also removing the classifier

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ import pandas as pd
 def main():
     df = pd.read_csv("data/raw/diabetes_cleaned.csv")
 
+    train_split_size = 0.7
+    # last column is the binary classifier
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:, -1].values.reshape(-1, 1)
+    
+    X_train = X[:int(train_split_size * len(X))]
+    y_train = y[:int(train_split_size * len(y))]
+    X_test = X[int(train_split_size * len(X)):]
+    y_test = y[int(train_split_size * len(y)):]
 
